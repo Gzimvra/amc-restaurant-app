3b98e373fd0aefacef3b1576233d5dd8cf03de8c
George Zimvragos <gzimvragos@gmail.com>
2025-03-10 14:56:46 +0200

    Backend: added graceful shutdown to server & handle the opening and closing of the database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-
 const cors = require("cors");
 const rateLimiter = require("./src/middleware/rateLimiter");
 const corsOptions = require("./src/middleware/corsOptions");
-const { connectDB } = require("./src/config/database");
+const { connectDB, closeDB } = require("./src/config/database");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
@@ -15,19 +14,15 @@ const PORT = process.env.PORT || 3500;
 
 // Apply the rate limiter to all routes
 app.use(rateLimiter);
-
-
-
-
 app.use(cors(corsOptions));
 
 // #################################################### ROUTES #####################################################
 
-// app.use("/login", require("./src/controllers/loginRoute"));
-// app.use("/register", require("./src/controllers/registerRoute"));
-// app.use("/restaurants", require("./src/controllers/restaurantsRoute"));
-// app.use("/reservations", require("./src/controllers/reservationsRoute"));
-// app.use("/user/reservations", require("./src/controllers/usersRoute"));
+// app.use("/login", require("./src/route/loginRoute"));
+// app.use("/register", require("./src/route/registerRoute"));
+// app.use("/restaurants", require("./src/route/restaurantsRoute"));
+// app.use("/reservations", require("./src/route/reservationsRoute"));
+// app.use("/user/reservations", require("./src/route/usersRoute"));
 
 app.all("*", (req, res) => {
   res.status(404);
@@ -38,5 +33,29 @@ app.all("*", (req, res) => {
   }
 });
 
+// #################################################### SERVER START/SHUTDOWN #####################################################
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+// Graceful Shutdown Function
+const shutdown = async (signal) => {
+  console.log(`\nReceived ${signal}. Shutting down...`);
+
+  if (server) {
+    server.close(() => {
+      console.log("Server closed.");
+      // closeDB().then(() => {
+      //   console.log("Database connection closed.");
+      // }).catch((err) => {
+      //   console.error("Error closing database:", err.message);
+      //   process.exit(1);
+      // });
+    });
+  }
+  
+  process.exit(0);
+};
+
+// Listen for termination signals
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+
+const server = app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
